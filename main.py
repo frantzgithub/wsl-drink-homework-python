9b068f9f5e4acc3f8874883d89e2d8ab49045242
Anky <anky@gmail.com>
2025-01-10 18:13:57 -0500

    change selled in sold

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
 last_year = 2024
 
-quantity_coca_selled = 150
-quantity_pepsi_selled = 150
+quantity_coca_sold = 150
+quantity_pepsi_sold = 150
 price_of_one_coca = 1.50
 price_of_one_pepsi = 1.50
 store_expenses = 250.0
 
-coca_revenue = quantity_coca_selled * price_of_one_coca
+coca_revenue = quantity_coca_sold * price_of_one_coca
 print("coca revenue is: " + str(coca_revenue))
-pepsi_revenue = quantity_pepsi_selled * price_of_one_pepsi
+pepsi_revenue = quantity_pepsi_sold * price_of_one_pepsi
 print("pepsi revenue is: " + str(pepsi_revenue))
 
 total_revenue = pepsi_revenue + coca_revenue
